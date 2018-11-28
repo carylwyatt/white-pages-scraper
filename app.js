31114fa4c35db9369f3134063719cc080284b9d4
carylwyatt <caryl.e.wyatt@gmail.com>
2018-11-27 22:20:32 -0500

    deleted comments

--- app.js
+++ app.js
@@ -11,15 +11,8 @@ const url = `https://whitepages.ivytech.edu/profile/${username}`;
 request({method: "GET", 
         "rejectUnauthorized": false,
         "url": url}, function (error, response, body) {
-  //        console.log(body);
-//        console.log(url);
-//        console.log(error);
-//        console.log('Response: ', response && response.statusCode);
-
-        //uncomment this console.log to test the form submission result 
-  //      //console.log('body:', body); // Print the form submission result
         const $ = cheerio.load(body);        
         const location = $('td:contains("Location")').next().text();
 
-        console.log(location);
+          console.log(location)
         });
